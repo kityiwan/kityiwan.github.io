f4e0f4ce4d7825c6aeb0e4a37e56955874ef85ba
kityiwan <kityi.wan.emily@gmail.com>
2023-05-29 11:48:42 -0400

    02-For Github Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Kit Yi Wan | Software Engineer';
+  title = 'portfolio';
 }
